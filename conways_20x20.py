52d48e9b12f085a5e76870d54b0937ed79801637
Jakub Stefan <kejkun1992@gmail.com>
2018-06-23 19:18:03 +0200

    20x20

---
+++ conways_20x20.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+ON = 255
+OFF = 0
+vals = [ON, OFF]
+
+def update(frame_num, img, grid, N):
+    new_grid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] +
+                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+            if grid[i, j] == ON:
+                if (total < 2) or (total > 3):
+                    new_grid[i, j] = OFF
+            else:
+                if total == 3:
+                    new_grid[i, j] = ON
+    img.set_data(new_grid)
+    grid[:] = new_grid[:]
+    return img
+
+
+def main():
+    N = 20
+    update_int = 100
+    grid = np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N),
+                                  frames=60, interval=update_int,
+                                  save_count=50)
+    plt.show()
+
+main()
